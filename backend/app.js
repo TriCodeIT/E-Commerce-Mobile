24dbc8f9e5546868fb55b8c0b774a557241fcbc2
TriCodeIT <bayu165kusuma@gmail.com>
2021-03-04 22:31:03 +0700

    enabling cors

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,33 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 require("dotenv/config");
 
-// const api = process.env.API_URL;
-
-
-// const productsRouter = require('./routers/products')
+//Cors
+app.use(cors());
+app.options('*', cors())
 
 //middleware
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
 
 //Routers
-app.use(`${api}/products`, productsRouter)
+const productsRoutes = require('./routers/products');
+const categoriesRoutes = require('./routers/categories');
+const ordersRoutes = require('./routers/orders');
+const usersRoutes = require('./routes/orders');
+
+const api = process.env.API_URL;
+
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+app.use(`${api}/users`, usersRoutes);
+
 
 //Database
 mongoose
@@ -34,6 +45,6 @@ mongoose
 
 //Server
 app.listen(3000, () => {
-  console.log(api);
+  
   console.log("server is running http://localhost:3000");
 });
